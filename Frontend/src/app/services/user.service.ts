e604524110c27572e35122cf5cf37c99e1168f05
samuraay <vtibii@gmail.com>
2018-12-06 16:36:00 +0100

    added working authentication with facebook

--- Frontend/src/app/services/user.service.ts
+++ Frontend/src/app/services/user.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/toPromise';
-import { AuthHttp } from 'angular2-jwt';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { User } from '../models/user.model';
 import { clientID } from '../config/constants';
 
 declare const FB: any;
 
-@Injectable({
-  providedIn: 'root'
-})
+export interface User {
+  img?: string;
+  email?: string;
+}
+
+@Injectable()
 export class UserService {
 
-  constructor(private http: AuthHttp) {
+  readonly URL = 'https://localhost:3000';
+  private currentUser: User;
+
+  constructor(private http: HttpClient) {
+    this.currentUser = null;
     FB.init({
       appId: clientID,
       status: false,
@@ -20,42 +27,61 @@ export class UserService {
     });
   }
 
-  fbLogin() {
-    return new Promise((resolve, reject) => {
-      FB.login(result => {
-        if (result.authResponse) {
-          return this.http.post(`http://localhost:3000/api/v1/auth/facebook`, {access_token: result.authResponse.accessToken})
-            .toPromise()
-            .then(response => {
-              const token = response.headers.get('x-auth-token');
-              if (token) {
-                localStorage.setItem('id_token', token);
-              }
-              resolve(response.json());
-            })
-            .catch(() => reject());
-        } else {
-          reject();
+  facebookLogin(): Promise<void> {
+    return FB.login((loginResponse) => {
+      if (loginResponse.authResponse) {
+        return this.login(loginResponse.authResponse);
+      } else {
+        return Promise.resolve(null);
+      }
+    }, {scope: 'public_profile, email'});
+  }
+
+  login(authResponse): Promise<void> {
+    return this.http.post(this.URL + '/auth/facebook', {access_token: authResponse.accessToken}, {observe: 'response'})
+      .toPromise()
+      .then((response: HttpResponse<Object>) => {
+        const token = response.headers.get('x-auth-token');
+        if (token) {
+          localStorage.setItem('id_token', token);
         }
-      }, {scope: 'public_profile, email'});
-    });
+      });
+  }
+
+  getUser(response: Response): Promise<User> {
+    console.log('Getting user: ', response);
+    return null;
   }
 
   logout() {
     localStorage.removeItem('id_token');
   }
 
-  isLoggedIn() {
-    return new Promise((resolve, reject) => {
-      this.getCurrentUser().then(user => resolve(true)).catch(() => reject(false));
-    });
+  isLoggedIn(): Promise<boolean> {
+    return this.getCurrentUser().then((user) => !!user);
   }
 
-  getCurrentUser() {
-    return new Promise((resolve, reject) => {
-      return this.http.get(`http://localhost:3000/api/v1/auth/me`).toPromise().then(response => {
-        resolve(response.json());
-      }).catch(() => reject());
+  getCurrentUser(): Promise<User> {
+    if (this.currentUser) {
+      return Promise.resolve(this.currentUser);
+    }
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'x-auth-token':  localStorage.getItem('id_token'),
+      })
+    };
+    console.log('Get current user: ', httpOptions);
+
+    return this.http.get(this.URL + '/auth/me', httpOptions)
+      .toPromise().then((response: Response) => {
+        'Current'
+        this.currentUser = new User(response['email'], response['firstName'], response['lastName']);
+        return this.currentUser;
+    }).catch(() => {
+        // todo reset views (log user out, go to homepage, show error message)
+        this.currentUser = null;
+        console.log('Error happened. Resetting views.');
+        return Promise.resolve(null);
     });
   }
 }
