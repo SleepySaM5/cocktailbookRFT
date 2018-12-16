bd08302a6359bd16861c65dbe1ee832cb844a0e4
samuraay <vtibii@gmail.com>
2018-12-16 20:34:25 +0100

    welcome page created

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Cocktail } from '../models/cocktail.model';
 import { CocktailService } from '../services/cocktail.service';
 import { User, UserService } from '../services/user.service';
+import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
@@ -15,7 +17,9 @@ export class HomeComponent {
   public loggedIn: boolean;
   user: User;
 
-  constructor(service: CocktailService, private authService: UserService) {
+  constructor(service: CocktailService,
+              private authService: UserService,
+              private router: Router) {
     service.getAllCocktails().subscribe( (cocktails: Cocktail[]) => {
       cocktails.forEach((cocktail) => console.log(cocktail.cocktailName));
       this.cocktails = cocktails;
@@ -28,5 +32,9 @@ export class HomeComponent {
     this.authService.getCurrentUser().then((user: any) => {
       this.user = user;
     });
+  }
+
+  getStarted(): void {
+    this.router.navigate(['/browse']);
   }
 }
